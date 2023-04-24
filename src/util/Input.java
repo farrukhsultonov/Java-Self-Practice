241965207b1b5982cb3e7c3653ed0539bcd0dd87
Farrukh Sultonov <farrukh.sultonov@codeup.com>
2023-04-24 13:41:24 -0500

    Movies part 3 of Arrays exercise finished

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,9 +3,14 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-    private static Scanner scanner = new Scanner(System.in);
+    public static Scanner scanner = new Scanner(System.in);
 
     public static String getString(){
+        return scanner.nextLine();
+    }
+
+    public String getString(String prompt){
+        System.out.println(prompt);
         return scanner.nextLine();
     }
 
